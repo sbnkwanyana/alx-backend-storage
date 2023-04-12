db9b2db5addfec7dc3d08fe3aeba71e2b7339457
Simphiwe Nkwanyana <sbnkwanyana@hotmail.com>
2023-04-12 23:25:45 +0200

    3. Storing lists

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -53,6 +53,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb(True)
 
+    @call_history
     @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """
