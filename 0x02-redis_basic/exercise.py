431c657a9a6a7d4f7ad544b9208c29fbb65c80f5
Simphiwe Nkwanyana <sbnkwanyana@hotmail.com>
2023-04-12 23:27:41 +0200

    3. Storing lists

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -8,6 +8,31 @@ import uuid
 from typing import Union, Callable, Any
 
 
+def replay(fn: Callable) -> None:
+    """
+    """
+    if fn is None or not hasattr(fn, '__self__'):
+        return
+    redis_store = getattr(fn.__self__, '_redis', None)
+    if not isinstance(redis_store, redis.Redis):
+        return
+    fxn_name = fn.__qualname__
+    in_key = '{}:inputs'.format(fxn_name)
+    out_key = '{}:outputs'.format(fxn_name)
+    fxn_call_count = 0
+    if redis_store.exists(fxn_name) != 0:
+        fxn_call_count = int(redis_store.get(fxn_name))
+    print('{} was called {} times:'.format(fxn_name, fxn_call_count))
+    fxn_inputs = redis_store.lrange(in_key, 0, -1)
+    fxn_outputs = redis_store.lrange(out_key, 0, -1)
+    for fxn_input, fxn_output in zip(fxn_inputs, fxn_outputs):
+        print('{}(*{}) -> {}'.format(
+            fxn_name,
+            fxn_input.decode("utf-8"),
+            fxn_output,
+        ))
+
+
 def call_history(method: Callable) -> Callable:
     """
     decorator function stores the inputs and outputs of a function
@@ -15,6 +40,7 @@ def call_history(method: Callable) -> Callable:
     @wraps(method)
     def invoker(self, *args, **kwargs) -> Any:
         """
+        stores both the input and output values of the function call
         """
         if isinstance(self._redis, redis.Redis):
             self._redis.rpush(f'{method.__qualname__}:inputs', str(args))
